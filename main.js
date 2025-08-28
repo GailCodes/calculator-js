345c97631f3d13a67bb6e48e7b86a0578e5517ab
Gail <80159413+GailCodes@users.noreply.github.com>
2025-08-28 15:32:30 -0700

    prevent enter key from submitting form

--- main.js
+++ main.js
@@ -135,6 +135,7 @@ window.addEventListener('keydown', (event) => {
     if (key >= '0' && key <= '9') {
         selectedSymbolOrNumber = parseInt(key);
     } else if (key == '=' || key == "Enter") {
+        event.preventDefault();
         calculateResult();
     } else {
         if (isLastOperationSymbol()) {
